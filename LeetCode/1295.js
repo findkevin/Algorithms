a78c344f303906a291b7c164ab57df2c03c8984e
Kevin Lam <find.keviin@gmail.com>
2019-12-23 18:51:56 -0500

    [FEAT] Completed LeetCode question 1295.

--- LeetCode/1295.js
+++ LeetCode/1295.js
@@ -28,3 +28,15 @@ Constraints:
 1 <= nums.length <= 500
 1 <= nums[i] <= 10^5
 */
+
+var findNumbers = function(nums) {
+  let counter = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i].toString().length % 2 === 0) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+console.log(findNumbers([12, 345, 2, 6, 7896])); //Output: 2
