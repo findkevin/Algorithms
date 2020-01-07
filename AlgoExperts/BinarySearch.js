431355f4d4a288ab03509888b1bb780844750e4a
Kevin Lam <find.keviin@gmail.com>
2020-01-07 16:09:41 -0500

    [FEAT] Completed AlgoExperts question Binary Search.

--- AlgoExperts/BinarySearch.js
+++ AlgoExperts/BinarySearch.js
@@ -2,9 +2,19 @@
 
 // Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to find if the target number is contained in the array and should return its index if it is, other -1.
 
-function binarySearch(array, target) {
-  //write your code here
-
+//Your solution below:
+function binarySearch(array, target, leftPointer = 0, rightPointer = array.length - 1) {
+  while (leftPointer <= rightPointer){
+    const midPoint = Math.floor((leftPointer + rightPointer) / 2);
+    if (target === array[midPoint]) {
+      return midPoint;
+    } else if (target < array[midPoint]) {
+      rightPointer = midPoint - 1;
+    } else {
+      leftPointer = midPoint + 1;
+    }
+  }
+  return -1;
 }
 
 // console.log(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33)) //3
