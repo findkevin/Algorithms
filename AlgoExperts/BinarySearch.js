128f6a2e15e3f74fc411a415649463dc4d56489b
Kevin Lam <find.keviin@gmail.com>
2020-01-08 23:26:09 -0500

    [FEAT] Included AlgoExperts solution #2.

--- AlgoExperts/BinarySearch.js
+++ AlgoExperts/BinarySearch.js
@@ -17,4 +17,25 @@ function binarySearch(array, target, leftPointer = 0, rightPointer = array.lengt
   return -1;
 }
 
+
+//AlgoExperts Solution #2 (Iterative)
+//O(log(n)) Time | O(1) Space
+// function binarySearch(array, target) {
+// 	return bSHelper(array, target, leftPointer = 0, rightPointer = array.length - 1)
+// }
+
+// function bSHelper(array, target, leftPointer, rightPointer){
+// 	while (leftPointer <= rightPointer){
+// 		const middlePointer = Math.floor((leftPointer + rightPointer) / 2);
+// 		if (array[middlePointer] === target){
+// 			return middlePointer;
+// 		} else if (array[middlePointer] > target){
+// 			rightPointer = middlePointer - 1;
+// 		} else {
+// 			leftPointer = middlePointer + 1;
+// 		}
+// 	}
+// 	return -1;
+// }
+
 // console.log(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33)) //3
