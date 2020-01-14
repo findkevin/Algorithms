9ae50d2e8f4fd586f3822eb8f72acdb03d0fd783
Kevin Lam <find.keviin@gmail.com>
2020-01-14 02:03:08 -0500

    [FEAT] Completed AlgoExpert question Find Closest Value in BST.

--- AlgoExperts/FindClosestValueinBST.js
+++ AlgoExperts/FindClosestValueinBST.js
@@ -11,6 +11,18 @@ function findClosestValueInBstHelper(tree, target, closest){
   if (tree === null){
     return closest;
   }
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)){
+    closest = tree.value;
+  }
+  if (target < tree.value){
+    return findClosestValueInBstHelper(tree.left, target, closest);
+  }
+  else if (target > tree.value){
+    return findClosestValueInBstHelper(tree.right, target, closest);
+  }
+  else {
+    return closest;
+  }
 }
 
 // findClosestValueInBst(tree, 100); //Output: 100
