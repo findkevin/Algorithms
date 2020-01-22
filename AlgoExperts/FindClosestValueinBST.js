6fa9e1d0bbe280cc4c0c78ff1c1421b0ba83aed9
Kevin Lam <find.keviin@gmail.com>
2020-01-22 15:14:08 -0500

    [FEAT] Added AlgoExpert solution#2 to FindClosestValueinBST.

--- AlgoExperts/FindClosestValueinBST.js
+++ AlgoExperts/FindClosestValueinBST.js
@@ -53,3 +53,30 @@ const test = new BST(100)
   .insert(57)
   .insert(60)
   .insert(4500)
+
+//AlgoExpert Solution #2
+//Average: O(log(n)) time | O(1) Space
+//Worst: O(n) time | O(1) Space
+/*
+  function findClosestValueInBst(tree, target) {
+    // Write your code here.
+    return findClosestValueInBstHelper(tree, target, Infinity);
+  }
+
+  function findClosestValueInBstHelper(tree, target, closest){
+    let currentNode = tree;
+    while (currentNode !== null) {
+      if (Math.abs(target - closest) > Math.abs(target - currentNode.value)){
+        closest = currentNode.value;
+      }
+      if (target < currentNode.value){
+        currentNode = currentNode.left;
+      } else if (target > currentNode.value){
+        currentNode = currentNode.right;
+      } else {
+        break;
+      }
+    }
+    return closest;
+  }
+*/
